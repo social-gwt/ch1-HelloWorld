6adf9bf3fe8e6d3e983a64f525016cbb48b442d4
jan.pumpe <jan.pumpe@VOSTRO14.fluidops.corp>
2013-08-30 16:14:08 +0200

    Social GWT chapter 1 - initial checkin

---
+++ SocialGWT-ch1-HelloWorld/src/com/socialgwt/client/SocialGWT.java
@@ -0,0 +1,15 @@
+package com.socialgwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class SocialGWT implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		RootPanel.get().add(new Label("abc"));
+
+	}
+
+}
